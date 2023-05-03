1a31c751aa43a72bcffe4cc56e09379e21838d0e
Maitlla <maitlla@gmail.com>
2023-05-04 00:20:41 +0200

    Autenticar en el back (probado con Insomnia)

---
+++ src/main/java/com/maitlla/talleres/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.maitlla.talleres.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .securityMatcher("/publico/compras/**")
+            .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+            
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
+
+    }
+
+    // @Bean
+    // public WebSecurityCustomizer webSecurityCustomizer() {
+    //     return (web) -> web.ignoring().requestMatchers("/**");
+    // }
+
+}
