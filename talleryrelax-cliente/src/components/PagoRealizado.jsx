8e738e2520ce8940deb90a433e40e598de214443
Maitlla <maitlla@gmail.com>
2023-05-07 16:46:14 +0200

    Añadir pantalla de pago hecho (sin uso por ahora)

---
+++ talleryrelax-cliente/src/components/PagoRealizado.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import '../App.css';
+import '../index.css';
+import '../views/PagoRealizado.css';
+
+const PagoRealizado = () => {
+
+
+    return (
+        <>
+            <h1>Pago realizado correctamente</h1>
+            <h4>Puedes ver tu compra en tu lista de Talleres</h4>
+            <Link
+                to={"/"} className="btn btn-4 btn-holder hover-border-7">
+                Mi lista de talleres
+            </Link>
+            <Link
+                to={"/"} className="btn btn-4 btn-holder hover-border-7">
+                Home
+            </Link>
+        </>
+    )
+
+};
+
+export default PagoRealizado;
